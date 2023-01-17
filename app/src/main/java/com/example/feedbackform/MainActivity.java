4525c96eecb2771e2606957063ebcecfb02e4a83
venkatnaresh <venkatnaresh234@gmail.com>
2023-01-17 12:28:39 +0530

    Changes made in the assigned Task.

--- app/src/main/java/com/example/feedbackform/MainActivity.java
+++ app/src/main/java/com/example/feedbackform/MainActivity.java
@@ -3,11 +3,23 @@ package com.example.feedbackform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.widget.AutoCompleteTextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputLayout name, phone, caste, subCaste, education, employment, epicNum, serialNum;
+    TextInputLayout partNum, boothName, wardName, constitution, city, council, address, postalNum;
+    TextInputLayout family, totalVotes, politicalParty, schemes;
+
+    TextInputEditText nameEt, phoneEt, epicNumEt, serialNumEt, partNumEt, addressEt;
+    TextInputEditText postalNumEt, totalVotesEt;
+
+    AutoCompleteTextView casteTv, subCasteTv, educationTv, employmentTv, boothNameTv, wardNameTv;
+    AutoCompleteTextView constitutionTv, cityTv, councilTv, familyTv, politicalPartyTv, schemesTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
